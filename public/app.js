4261bac9fb7909d0e72de8afe3c1ff64b5aae8ae
faraaz baig <fazzy0908@yandex.com>
2021-03-01 18:12:36 +0530

    classes in typescript

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
 "use strict";
+//classes
+var Invoice = /** @class */ (function () {
+    //readonly client: string
+    //private details: string
+    //public amount: number
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes Rs " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('faraaz', 'pizza bill', 250);
+var invTwo = new Invoice('ashfaq', 'subway bill', 2850);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector('.new-item-form');
 // inputs
 var type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+//classes
+class Invoice {
+   //readonly client: string
+    //private details: string
+    //public amount: number
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ) {}
+    format() {
+        return `${this.client} owes Rs ${this.amount} for ${this.details}`
+    }
+}
+
+
+const invOne = new Invoice('faraaz', 'pizza bill', 250)
+const invTwo = new Invoice('ashfaq', 'subway bill', 2850)
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format())
+})
+
+
 const form = document.querySelector('.new-item-form') as HTMLFontElement
 
 // inputs
