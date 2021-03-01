d1ed035fe501679fc0edf254718909a86d41a3b8
faraaz baig <fazzy0908@yandex.com>
2021-03-01 16:38:04 +0530

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,20 @@
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+//arrays
+var ninjas = [];
+ninjas = ['yoshi', 'faraaz'];
+ninjas.push('ayman');
+ninjas.pop();
+// union types
+var mixed = [];
+mixed.push(20);
+mixed.push(false);
+mixed.push('faraaz');
+console.log(mixed);
+var uid;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
