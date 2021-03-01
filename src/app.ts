6af946c2878e72168b019cabb9a1cacecf64cdf4
faraaz baig <fazzy0908@yandex.com>
2021-03-01 17:50:54 +0530

    DOM with TS

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+const form = document.querySelector('.new-item-form') as HTMLFontElement
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement
+const toform = document.querySelector('#tofrom') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+
+    console.log(
+       type.value,
+       toform.value,  
+       details.value, 
+       amount.valueAsNumber, 
+    )
+})
