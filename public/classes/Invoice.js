41c6d8e8b082b2d8279da6aea2a0d85fdb09177f
faraaz baig <fazzy0908@yandex.com>
2021-03-01 19:19:57 +0530

    full TS app done

--- public/classes/Invoice.js
+++ public/classes/Invoice.js
@@ -1,4 +1,3 @@
-//classes
 export class Invoice {
     constructor(client, details, amount) {
         this.client = client;
@@ -6,6 +5,6 @@ export class Invoice {
         this.amount = amount;
     }
     format() {
-        return `${this.client} owes Rs ${this.amount} for ${this.details}`;
+        return `${this.client} owes £${this.amount} for ${this.details}`;
     }
 }
--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,12 +1,13 @@
-//classes
-export class Invoice {
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ) {}
-    format() {
-        return `${this.client} owes Rs ${this.amount} for ${this.details}`
-    }
-}
+import { HasFormatter } from '../interfaces/HasFormatter.js';
 
+export class Invoice implements HasFormatter {
+  constructor(
+    readonly client: string, 
+    private details: string, 
+    public amount: number,
+  ){}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
